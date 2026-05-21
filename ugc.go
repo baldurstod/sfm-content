7fa511e20f866c125a494d4536a543b1e1313316
Guillaume <108530065+baldurstod@users.noreply.github.com>
2026-05-21 21:09:25 +0200

    Added zip extraction + dl file

--- ugc.go
+++ ugc.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"archive/zip"
+	"bufio"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -9,6 +10,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
+	"path/filepath"
 	"regexp"
 	"runtime/debug"
 	"strconv"
@@ -36,18 +39,15 @@ func ugcHandler(c *gin.Context) {
 
 func ugcManifestHandler(c *gin.Context) {
 	id := c.Param("id")
-	log.Println("ugcManifestHandler", c.Request.URL, id)
 
-	path := getFilePath(id) + "manifest.json"
+	path := getManifestPath(id)
 
 	_, err := os.Stat(path)
 
 	notExist := errors.Is(err, os.ErrNotExist)
 
-	log.Println("notExist", notExist)
-
 	if err != nil && !notExist {
-		log.Println("ugcManifestHandler", err)
+		log.Println("error in ugcManifestHandler accessing path "+path, err)
 		c.String(http.StatusInternalServerError, "")
 		return
 	}
@@ -66,7 +66,7 @@ func ugcManifestHandler(c *gin.Context) {
 		}
 	}
 
-	content, err := os.ReadFile(getFilePath(id) + "manifest.json")
+	content, err := os.ReadFile(getManifestPath(id))
 	if err != nil {
 		log.Println("failed to read manifest for item "+id, err)
 		c.String(http.StatusInternalServerError, "")
@@ -76,9 +76,56 @@ func ugcManifestHandler(c *gin.Context) {
 }
 
 func ugcFileHandler(c *gin.Context) {
+	id := c.Param("id")
+	path := c.Param("path")
+	//path = "models/mysterypancake/../../....\\..\\/../../..\\androidlogo/android.mdl"
+	//path = "/../getprintfulproducts.json"
+
+	// Cleanup the path
+	path = filepath.Clean("." + path)
+
+	// Check if the path is local
+	if !filepath.IsLocal(path) {
+		log.Println("path is not local " + path + " for item " + id)
+		c.String(http.StatusInternalServerError, "")
+		return
+	}
+
+	log.Println(path)
+
+	_, err := os.Stat(path)
+
+	notExist := errors.Is(err, os.ErrNotExist)
+
+	if err != nil && !notExist {
+		log.Println("error in ugcFileHandler accessing path "+path, err)
+		c.String(http.StatusInternalServerError, "")
+		return
+	}
+
+	if notExist {
+		err = extractFile(id, path)
+		if err != nil {
+			log.Println("error in ugcFileHandler "+path, err)
+			c.String(http.StatusInternalServerError, "")
+			return
+		}
+	}
+
+	completePath := filepath.Join(getFilesPath(id), path)
+
+	content, err := os.ReadFile(completePath)
+	if err != nil {
+		log.Println("failed to read path +"+path+" for item "+id, err)
+		c.String(http.StatusInternalServerError, "")
+	}
+
+	c.Data(http.StatusOK, "application/octet-stream", content)
+
+	//c.Data(http.StatusOK, "application/json", content)
 }
 
-func getFilePath(id string) string {
+func getItemPath(id string) string {
 	re := regexp.MustCompile(`(.{2}|.{1})`)
 	x := re.FindAllStringSubmatch(id, -1)
 
@@ -91,8 +138,20 @@ func getFilePath(id string) string {
 	return path
 }
 
+func getManifestPath(id string) string {
+	return getItemPath(id) + "manifest.json"
+}
+
+func getZipPath(id string) string {
+	return getItemPath(id) + id + ".zip"
+}
+
+func getFilesPath(id string) string {
+	return path.Join(getItemPath(id), "files")
+}
+
 func downloadFileIfNotExist(id string) error {
-	_, err := os.Stat(getFilePath(id) + id + ".zip")
+	_, err := os.Stat(getZipPath(id))
 
 	notExist := errors.Is(err, os.ErrNotExist)
 	if notExist {
@@ -125,12 +184,51 @@ func downloadFile(id string) error {
 		return fmt.Errorf("failed to read response body for item "+id+": <%w>", err)
 	}
 
-	err = os.MkdirAll(getFilePath(id), 0755)
+	err = os.MkdirAll(getFilesPath(id), 0755)
 	if err != nil {
-		return fmt.Errorf("failed to create folders for item "+id+" with path "+getFilePath(id)+": <%w>", err)
+		return fmt.Errorf("failed to create folders for item "+id+" with path "+getFilesPath(id)+": <%w>", err)
 	}
 
-	err = os.WriteFile(getFilePath(id)+id+".zip", buf, 0666)
+	err = os.WriteFile(getZipPath(id), buf, 0666)
+	if err != nil {
+		return fmt.Errorf("failed to write file for item "+id+": <%w>", err)
+	}
+
+	return nil
+}
+
+func extractFile(id string, p string) error {
+	err := downloadFileIfNotExist(id)
+	if err != nil {
+		return fmt.Errorf("failed to extract file for item "+id+": <%w>", err)
+	}
+
+	reader, err := zip.OpenReader(getZipPath(id))
+	if err != nil {
+		return fmt.Errorf("failed to open zip for item "+id+": <%w>", err)
+	}
+	defer reader.Close()
+
+	file, err := reader.Open(p)
+	if err != nil {
+		return fmt.Errorf("failed to open zip file "+p+" for item "+id+": <%w>", err)
+	}
+
+	defer file.Close()
+
+	buf, err := io.ReadAll(bufio.NewReader(file))
+	if err != nil {
+		return fmt.Errorf("failed to read zip file "+p+" for item "+id+": <%w>", err)
+	}
+
+	completePath := filepath.Join(getFilesPath(id), p)
+
+	err = os.MkdirAll(path.Dir(completePath), 0755)
+	if err != nil {
+		return fmt.Errorf("failed to create folders for item "+id+" with path "+completePath+": <%w>", err)
+	}
+
+	err = os.WriteFile(completePath, buf, 0666)
 	if err != nil {
 		return fmt.Errorf("failed to write file for item "+id+": <%w>", err)
 	}
@@ -139,7 +237,7 @@ func downloadFile(id string) error {
 }
 
 func generateItemManifest(id string) error {
-	reader, err := zip.OpenReader(getFilePath(id) + id + ".zip")
+	reader, err := zip.OpenReader(getZipPath(id))
 	if err != nil {
 		return fmt.Errorf("failed to open zip for item "+id+": <%w>", err)
 	}
@@ -147,7 +245,7 @@ func generateItemManifest(id string) error {
 
 	files := []string{}
 	for _, file := range reader.File {
-		if true || strings.HasSuffix(file.Name, ".mdl") {
+		if strings.HasSuffix(file.Name, ".mdl") || strings.HasSuffix(file.Name, ".pcf") {
 			files = append(files, file.Name)
 		}
 	}
@@ -156,7 +254,7 @@ func generateItemManifest(id string) error {
 	if err != nil {
 		return fmt.Errorf("failed to marshal manifest for item "+id+": <%w>", err)
 	}
-	err = os.WriteFile(getFilePath(id)+"manifest.json", j, 0666)
+	err = os.WriteFile(getManifestPath(id), j, 0666)
 	if err != nil {
 		return fmt.Errorf("failed to write manifest for item "+id+": <%w>", err)
 	}
@@ -181,7 +279,7 @@ func generateManifest(files []string) map[string]any {
 				current = c.(map[string]any)
 			} else {
 				if i == l-1 {
-					current[segment] = nil
+					current[segment] = 1
 				} else {
 					c := map[string]any{}
 					current[segment] = c
